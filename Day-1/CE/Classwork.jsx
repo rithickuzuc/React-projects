a92e15d4670db305080edcc17781bfdfa57d943e
Rithick Parthiban <145524980+rithickuzuc@users.noreply.github.com>
2024-07-31 16:31:21 +0530

    Update Classwork.jsx

--- Day-1/CE/Classwork.jsx
+++ Day-1/CE/Classwork.jsx
@@ -1,1 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+function alertMsg()
+{
+   alert("Message from Javascript Alert!");
+   console.log("Message to developer");
+}
+function App() {
+  return (
+    <div>
+      <h1>Let we see the output of JAVASCRIPT</h1>
+      <button onClick={alertMsg}>Click</button>
+    </div>
+  );
+}
+
+export default App;
 
